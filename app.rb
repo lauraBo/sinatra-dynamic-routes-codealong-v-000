a2889adbae9c5323a61e5e6e04b592a670d2bf0a
Laura Bodell <bodelllaura@gmail.com>
2018-05-01 19:42:13 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -18,5 +18,9 @@ class App < Sinatra::Base
   get "/goodbye/:name" do  
   @user_name = params[:name]
   "Goodbye,#{@user_name}"
-  end   
+  end 
+  
+  get '/multiply/:num1/:num2' do
+    @num1 = params[:num1]
+    @num2 = params [:num2]
 end
